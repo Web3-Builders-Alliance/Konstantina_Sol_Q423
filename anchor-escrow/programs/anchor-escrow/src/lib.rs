c06f9589be27c38fa355817a945cf779c1185ba6
konstantinagian <102831866+konstantinagian@users.noreply.github.com>
2023-12-12 13:06:37 +0200

    Add refund implementation in escrow

--- anchor-escrow/programs/anchor-escrow/src/lib.rs
+++ anchor-escrow/programs/anchor-escrow/src/lib.rs
@@ -1,5 +1,5 @@
 use anchor_lang::prelude::*;
-use anchor_spl::{token::{Mint, TokenAccount, Token, Transfer, transfer}, associated_token::AssociatedToken};
+use anchor_spl::{token::{Mint, TokenAccount, Token, Transfer, transfer, CloseAccount, close_account}, associated_token::AssociatedToken};
 
 declare_id!("9obik1Tntr8BDeoLj7KwnG7e6uQSDeqpX3s8WMeFUYeJ");
 
@@ -16,6 +16,7 @@ pub mod anchor_escrow {
             taker_token: ctx.accounts.mint_b.key(),
             receive_amount,
             bump: ctx.bumps.escrow,
+            vault_bump: ctx.bumps.vault
         });
 
         let accounts = Transfer {
@@ -35,9 +36,51 @@ pub mod anchor_escrow {
     //     Ok(())
     // }
 
-    // pub fn refund(ctx: Context<Refund>) -> Result<()> {
-    //     Ok(())
-    // }
+    pub fn refund(ctx: Context<Refund>) -> Result<()> {
+        // we don't need seed because it's saved
+        // we don't need deposit or receive amounts because we can find what amount is in the vault
+
+        // refund: vault -> maker
+        let accounts = Transfer {
+            from: ctx.accounts.vault.to_account_info(),
+            to: ctx.accounts.maker_ata_a.to_account_info(),
+            authority: ctx.accounts.escrow.to_account_info()
+        };
+
+        let binding = ctx.accounts.escrow.seed.to_le_bytes();
+
+        let signer_seeds = [
+            &[
+                b"escrow",
+                ctx.accounts.maker.to_account_info().key.as_ref(),
+                binding.as_ref()
+            ][..]
+        ];
+
+        // ! CpiContext::new_with_signer - use when signing on behalf of a PDA
+        let cpi_ctx = CpiContext::new_with_signer(
+            ctx.accounts.token_program.to_account_info(),
+            accounts,
+            &signer_seeds
+        );
+
+        transfer(cpi_ctx, ctx.accounts.vault.amount)?;
+
+        // We now are gonna close the vault too
+        let close_accounts = CloseAccount {
+            account: ctx.accounts.vault.to_account_info(),
+            destination: ctx.accounts.maker.to_account_info(), // where to send the lamports from closing this account
+            authority: ctx.accounts.escrow.to_account_info()
+        };
+
+        let cpi_ctx = CpiContext::new_with_signer( // example of shadowing
+            ctx.accounts.token_program.to_account_info(),
+            close_accounts,
+            &signer_seeds // same signer seeds because same signer
+        );
+
+        close_account(cpi_ctx)
+    }
 }
 
 #[derive(Accounts)]
@@ -50,27 +93,69 @@ pub struct Make <'info>{
     mint_b: Account<'info, Mint>,
 
     #[account(
-        init,
-        payer=maker,
+        // init, ... maker_ata should already exist if thety're making
+        // payer=maker,
+        mut,
         associated_token::mint = mint_a, // guaranteed to match mint_a
         associated_token::authority = maker
     )]
     maker_ata_a: Account<'info, TokenAccount>,
 
     #[account(
+        // init_if_needed, // although normally it shouldn't exist
         init,
         payer=maker,
-        associated_token::mint = mint_a,
-        associated_token::authority = escrow
+        seeds = [b"vault", escrow.key().as_ref()], // now we don't have to put the seed in
+        // by using the escrow key we're inheriting the entropy of the seed
+        bump,
+        token::mint = mint_a, // why not an associated token here?
+        token::authority = escrow, // why not an associated token here?
+        // we would even set the authority to vault
     )]
     vault: Account<'info, TokenAccount>,
 
     #[account(
         init,
+        payer = maker,
+        space = Escrow::INIT_SPACE,
         seeds = [b"escrow".as_ref(), maker.key().as_ref(), seed.to_le_bytes().as_ref()],
         bump,
-        space = Escrow::INIT_SPACE,
-        payer = maker
+    )]
+    escrow: Account<'info, Escrow>, // an account storing our escrow details
+
+    associated_token_program: Program<'info, AssociatedToken>, // needed because our escrow uses spl tokens
+    token_program: Program<'info, Token>, // wouldn't be needed if we were just using sol
+    system_program: Program<'info, System> // must have system program if we're initializing any account
+}
+
+#[derive(Accounts)]
+pub struct Refund <'info>{
+    #[account(mut)]
+    maker: Signer<'info>, // signer
+
+    mint_a: Account<'info, Mint>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint_a,
+        associated_token::authority = maker
+    )]
+    maker_ata_a: Account<'info, TokenAccount>,
+
+    #[account(
+        seeds = [b"vault", escrow.key().as_ref()],
+        bump = escrow.vault_bump,
+        token::mint = mint_a,
+        token::authority = escrow
+    )]
+    vault: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        close = maker, // Marks the account as closed at the end of the instruction’s execution and sends its lamports to the specified account
+        // we still need to provide the seed constraints even though the account is initialized
+        seeds = [b"escrow".as_ref(), maker.key().as_ref(), escrow.seed.to_le_bytes().as_ref()], // now we didn't pass seed by instruction but it's saved in the Escrow struct
+        bump = escrow.bump,
     )]
     escrow: Account<'info, Escrow>,
 
@@ -86,8 +171,9 @@ pub struct Escrow {
     taker_token: Pubkey,
     receive_amount: u64,
     bump: u8,
+    vault_bump: u8 // what if we don't add this -> we could but we're saving compute
 }
 
 impl Space for Escrow {
-    const INIT_SPACE: usize = 32 + 8 + 32 + 1 + 8 + 8; // anchor adds a discriminator of 8 bytes
+    const INIT_SPACE: usize = 32 + 8 + 32 + 1 + 1 + 8 + 8; // anchor adds a discriminator of 8 bytes
 }
